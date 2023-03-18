2711d4f82e333180ff2b2f755c66b4418089c37f
yilin <james.lin1065@gmail.com>
2023-03-17 23:30:02 -0400

    test cicd 3

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping(path = "/")
     public String healthcheck() {
-        return "OK 200 ✅";
+        return "OK 200 ✅✅";
     }
 }
